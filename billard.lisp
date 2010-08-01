3aa613d739cbadf625cbb158f87eee54e3739730
Rolando Pereira <finalyugi@sapo.pt>
2010-08-01 17:07:51 +0100

    Added functions to move and draw the ball on the screen

--- billard.lisp
+++ billard.lisp
@@ -1,6 +1,6 @@
 (require 'lisprebuilder-sdl)
 
-(defstruct ball x y r direction-x direction-y)
+(defstruct ball x y r direction-x direction-y vel-x vel-y)
 
 (defun invert-direction-ball (ball &key x y)
   "Invert the direction of a ball.
@@ -12,16 +12,21 @@ ball should invert."
   (when y
     (setf (ball-direction-y ball) (* -1 (ball-direction-y ball)))))
 
+(defun move-ball (ball)
+  (incf (ball-x ball) (* (ball-vel-x ball) (ball-direction-x ball)))
+  (incf (ball-y ball) (* (ball-vel-y ball) (ball-direction-y ball))))
+
+(defun draw-ball (ball)
+  (sdl:draw-circle-* (ball-x ball) (ball-y ball) (ball-r ball)))
+  
 (defun billard ()
-  (let ((bola (make-ball :x 50 :y 50 :r 10 :direction-x 1 :direction-y 1))
+  (let ((bola (make-ball :x 50 :y 50 :r 10 :direction-x 1 :direction-y 1 :vel-x 5 :vel-y 5))
          (window-width 800)
-         (window-height 600)
-         (vel-x 10)
-         (vel-y 10))
+         (window-height 600))
     (sdl:with-init ()
       (sdl:window window-width window-height :position t
         :title-caption "Billard")
-      (sdl:draw-circle-* (ball-x bola) (ball-y bola) (ball-r bola))
+      (draw-ball bola)
       (sdl:with-events ()
         (:quit-event () t)
         (:key-down-event ()
@@ -29,15 +34,14 @@ ball should invert."
             (sdl:push-quit-event)))
         (:idle ()
           (sdl:clear-display sdl:*black*)
-          (incf (ball-x bola) (* vel-x (ball-direction-x bola)))
-          (incf (ball-y bola) (* vel-y (ball-direction-y bola)))
           (when (or (> (+ (ball-y bola) (ball-r bola)) window-height)
                   (< (ball-y bola) 0))
             (invert-direction-ball bola :y 1))
           (when (or (> (+ (ball-x bola) (ball-r bola)) window-width)
                   (< (ball-x bola) 0))
             (invert-direction-ball bola :x 1))
-          (sdl:draw-circle-* (ball-x bola) (ball-y bola) (ball-r bola))
+          (move-ball bola)
+          (draw-ball bola)
           (sdl:update-display))))))
 
         
