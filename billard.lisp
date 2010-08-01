dc165f394942cd4d2d632d24ed859d65fbd5da1c
Rolando Pereira <finalyugi@sapo.pt>
2010-08-01 17:36:50 +0100

    Added a color parameter to the ball and functions move and draw that work on lists of balls

--- billard.lisp
+++ billard.lisp
@@ -1,6 +1,6 @@
 (require 'lisprebuilder-sdl)
 
-(defstruct ball x y r direction-x direction-y vel-x vel-y)
+(defstruct ball x y r direction-x direction-y vel-x vel-y color)
 
 (defun invert-direction-ball (ball &key x y)
   "Invert the direction of a ball.
@@ -16,17 +16,27 @@ ball should invert."
   (incf (ball-x ball) (* (ball-vel-x ball) (ball-direction-x ball)))
   (incf (ball-y ball) (* (ball-vel-y ball) (ball-direction-y ball))))
 
+(defun move-balls (list-balls)
+  (dolist (ball list-balls)
+    (move-ball ball)))
+
 (defun draw-ball (ball)
-  (sdl:draw-circle-* (ball-x ball) (ball-y ball) (ball-r ball)))
+  (sdl:draw-circle-* (ball-x ball) (ball-y ball) (ball-r ball) :color (ball-color ball)))
+
+(defun draw-balls (list-balls)
+  (dolist (ball list-balls)
+    (draw-ball ball)))
   
 (defun billard ()
-  (let ((bola (make-ball :x 50 :y 50 :r 10 :direction-x 1 :direction-y 1 :vel-x 5 :vel-y 5))
+  (let* ((bola1 (make-ball :x 50 :y 50 :r 10 :direction-x 1 :direction-y 1 :vel-x 5 :vel-y 5 :color sdl:*yellow*))
+         (bola2 (make-ball :x 300 :y 220 :r 10 :direction-x -1 :direction-y -1 :vel-x 5 :vel-y 5 :color sdl:*cyan*))
+          (bolas (list bola1 bola2))
          (window-width 800)
          (window-height 600))
     (sdl:with-init ()
       (sdl:window window-width window-height :position t
         :title-caption "Billard")
-      (draw-ball bola)
+      (draw-balls bolas)
       (sdl:with-events ()
         (:quit-event () t)
         (:key-down-event ()
@@ -34,14 +44,20 @@ ball should invert."
             (sdl:push-quit-event)))
         (:idle ()
           (sdl:clear-display sdl:*black*)
-          (when (or (> (+ (ball-y bola) (ball-r bola)) window-height)
-                  (< (ball-y bola) 0))
-            (invert-direction-ball bola :y 1))
-          (when (or (> (+ (ball-x bola) (ball-r bola)) window-width)
-                  (< (ball-x bola) 0))
-            (invert-direction-ball bola :x 1))
-          (move-ball bola)
-          (draw-ball bola)
+
+          ;(maybe-change-direction-ball bola1)
+          ;; TODO: This should probably be a macro, but it also works like this
+          (mapcar #'(lambda (ball)
+                     (when (or (> (+ (ball-y ball) (ball-r ball)) window-height)
+                             (< (ball-y ball) 0))
+                       (invert-direction-ball ball :y 1))
+                     (when (or (> (+ (ball-x ball) (ball-r ball)) window-width)
+                             (< (ball-x ball) 0))
+                       (invert-direction-ball ball :x 1)))
+            bolas)
+
+          (move-balls bolas)
+          (draw-balls bolas)
           (sdl:update-display))))))
 
         
